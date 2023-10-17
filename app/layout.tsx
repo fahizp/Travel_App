026b2effec35fb0b492536300b3c036d2eb5afba
fahizp <fahizp3@gmail.com>
2023-10-17 19:34:25 +0530

    feat: complete the nav bar ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+
 import './globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Travel',
   description: 'Travel App for Camping',
 }
-
 
 export default function RootLayout({
   children,
@@ -16,12 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body >
-        <Navbar/>
-        <main className='relative overflow-hidden'>
-        {children}
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">
+          {children}
         </main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   )
